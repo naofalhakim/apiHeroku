ad1b8c83290fe11f0d61daa1155cfbf567e89af1
naofalhakim <hakimnaofal@gmail.com>
2020-09-04 21:52:31 +0700

    first init

---
+++ detail_berita.php
@@ -0,0 +1,36 @@
+<?php
+// include file koneksi
+require 'koneksi.php';
+// buat QUery perintah untuk menampilkan semua data
+// Secara Descending berdasarkan ID
+if(isset($_GET['id_berita'])){
+	$idBerita = $_GET['id_berita'];
+
+	$sql_get_berita = "SELECT * FROM tb_berita where id = $idBerita";
+	$query = $conn->query($sql_get_berita);
+
+	// Variable penampung array sementara
+	$response_data = null;
+
+	while ($data = $query->fetch_assoc()) {
+		// tambahkan data yg di seleksi ke dalam array
+		$response_data[] = $data;
+	}
+
+	// Cek apakah datanya null ?
+	if (is_null($response_data)) {
+		// jika ya, buat status untuk response jadi false
+		$status = false;
+	} else {
+		// jika tidak, buat status untuk response jadi true
+		$status = true;
+	}
+	// Set type header response ke Json
+	header('Content-Type: application/json');
+	// Bungkus data dalam array
+	$response = ['status' => $status, 'berita' => $response_data];
+	// tampilkan dan convert ke format json
+	echo json_encode($response);
+}else{
+	echo "<b>Hasil pencarian : tidak ditemukan</b>";
+}
---
+++ search_berita.php
@@ -0,0 +1,37 @@
+<?php
+// include file koneksi
+require 'koneksi.php';
+// buat QUery perintah untuk menampilkan semua data
+// Secara Descending berdasarkan ID
+// $_GET['cari'] = "madura";
+if(isset($_GET['cari'])){
+	$cari = $_GET['cari'];
+
+	$sql_get_berita = "SELECT * FROM tb_berita where judul_berita like '%".$cari."%' or isi_berita like '%".$cari."%' or penulis like '%".$cari."%' ";
+	$query = $conn->query($sql_get_berita);
+
+	// Variable penampung array sementara
+	$response_data = null;
+
+	while ($data = $query->fetch_assoc()) {
+		// tambahkan data yg di seleksi ke dalam array
+		$response_data[] = $data;
+	}
+
+	// Cek apakah datanya null ?
+	if (is_null($response_data)) {
+		// jika ya, buat status untuk response jadi false
+		$status = false;
+	} else {
+		// jika tidak, buat status untuk response jadi true
+		$status = true;
+	}
+	// Set type header response ke Json
+	header('Content-Type: application/json');
+	// Bungkus data dalam array
+	$response = ['status' => $status, 'berita' => $response_data];
+	// tampilkan dan convert ke format json
+	echo json_encode($response);
+}else{
+	echo "<b>Hasil pencarian : Tidak ditemukan</b>";
+}
